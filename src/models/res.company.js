f02888d4148cd861c1acc9464626286315c5b324
bjo163 <33343513+bjo163@users.noreply.github.com>
2026-09-13 19:05:24 +0700

    feat: enforce explicit API exposure policy

--- src/models/res.company.js
+++ src/models/res.company.js
@@ -4,6 +4,19 @@ import { fields } from '../orm/index.js'
 export class ResCompany {
   static _name = 'res.company'
   static _description = 'Company'
+  static _api = {
+    rest: true,
+    rpc: [
+    'search',
+    'search_read',
+    'count',
+    'browse',
+    'create',
+    'create_many',
+    'write',
+    'unlink'
+  ]
+  }
   static table = companies
 
   static fields = {
