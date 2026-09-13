7a279daa49b519a1d7cdf8122aee6d555a4bde71
bjo163 <33343513+bjo163@users.noreply.github.com>
2026-09-13 19:06:05 +0700

    feat: explicitly expose MVP models through API

--- src/models/res.partner.js
+++ src/models/res.partner.js
@@ -4,6 +4,10 @@ import { fields } from '../orm/index.js'
 export class ResPartner {
   static _name = 'res.partner'
   static _description = 'Partner'
+  static _api = {
+    rest: true,
+    rpc: ['search', 'search_read', 'count', 'browse', 'create', 'create_many', 'write', 'unlink']
+  }
   static table = partners
 
   static fields = {
