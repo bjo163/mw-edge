29887e7699c66f6f2bf7fa61f39e14f03872f8aa
bjo163 <33343513+bjo163@users.noreply.github.com>
2026-09-13 18:23:25 +0700

    feat: harden ORM model contracts

--- src/orm/fields.js
+++ src/orm/fields.js
@@ -1,14 +1,22 @@
 function field(type, options = {}) {
+  if (options === undefined) options = {}
+  if (options === null || typeof options !== 'object' || Array.isArray(options)) {
+    throw new TypeError('Field options must be an object')
+  }
+
   return Object.freeze({ type, ...options })
 }
 
-export const fields = {
-  Char: options => field('char', options),
-  Text: options => field('text', options),
-  Integer: options => field('integer', options),
-  Float: options => field('float', options),
-  Boolean: options => field('boolean', options),
-  DateTime: options => field('datetime', options),
-  Selection: (selection, options = {}) => field('selection', { selection, ...options }),
-  Many2one: (comodel, options = {}) => field('many2one', { comodel, ...options })
-}
+export const fields = Object.freeze({
+  Char: (options = {}) => field('char', options),
+  Text: (options = {}) => field('text', options),
+  Integer: (options = {}) => field('integer', options),
+  Float: (options = {}) => field('float', options),
+  Boolean: (options = {}) => field('boolean', options),
+  DateTime: (options = {}) => field('datetime', options),
+  Selection: (selection, options = {}) => field('selection', {
+    ...options,
+    selection: Object.freeze([...selection])
+  }),
+  Many2one: (comodel, options = {}) => field('many2one', { ...options, comodel })
+})
