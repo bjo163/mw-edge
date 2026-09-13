f4df248cdc5582968249f904dff3698923099010
bjo163 <33343513+bjo163@users.noreply.github.com>
2026-09-13 18:53:44 +0700

    test: verify D1 migrations are idempotent

--- test/integration/apply-migrations.js
+++ test/integration/apply-migrations.js
@@ -2,3 +2,4 @@ import { env } from 'cloudflare:workers'
 import { applyD1Migrations } from 'cloudflare:test'
 
 await applyD1Migrations(env.MW_DB, env.TEST_MIGRATIONS)
+await applyD1Migrations(env.MW_DB, env.TEST_MIGRATIONS)
