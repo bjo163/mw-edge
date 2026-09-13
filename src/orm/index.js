1c243c43b3bf196bde468e272c86a7cf2a0bd413
bjo163 <33343513+bjo163@users.noreply.github.com>
2026-09-13 18:23:31 +0700

    feat: harden ORM model contracts

--- src/orm/index.js
+++ src/orm/index.js
@@ -1,3 +1,9 @@
 export { fields } from './fields.js'
 export { ModelRegistry } from './registry.js'
 export { createEnvironment } from './environment.js'
+export {
+  MwError,
+  ValidationError,
+  NotFoundError,
+  ConflictError
+} from './errors.js'
