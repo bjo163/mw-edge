1dd097bb10d12e7e8134cec4034a96194d71bb9b
bjo163 <33343513+bjo163@users.noreply.github.com>
2026-09-13 17:58:14 +0700

    feat: add MW Edge MVP foundation

---
+++ src/models/index.js
@@ -0,0 +1,7 @@
+import { ModelRegistry } from '../orm/index.js'
+import { ResCompany } from './res.company.js'
+import { ResPartner } from './res.partner.js'
+
+export const registry = new ModelRegistry()
+  .register(ResCompany)
+  .register(ResPartner)
